e190192b5637185b3580d79bce572d86b2c65e79
Patel Dhruvin Pravinbhai <dhruvinpatel1831@gmail.com>
2020-10-17 01:29:20 +0530

    initial commit

---
+++ src/Header.js
@@ -0,0 +1,43 @@
+import React from "react";
+import "./Header.css";
+import SearchIcon from "@material-ui/icons/Search";
+import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
+
+function Header() {
+  return (
+    <div className="header">
+      <img
+        className="header__logo"
+        src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+        alt="Amazon"
+      />
+
+      <div className="header__search">
+        <input type="text" className="header__searchInput" />
+        <SearchIcon className="header__searchIcon" />
+      </div>
+
+      <div className="header__nav">
+        <div className="header__option">
+          <span className="header__optionLineOne">Hello! Guest</span>
+          <span className="header__optionLineTwo">Sign In</span>
+        </div>
+        <div className="header__option">
+          <span className="header__optionLineOne">Returns</span>
+          <span className="header__optionLineTwo">& Orders</span>
+        </div>
+        <div className="header__option">
+          <span className="header__optionLineOne">Your</span>
+          <span className="header__optionLineTwo">Prime</span>
+        </div>
+
+        <div className="header__optionBasket">
+          <ShoppingCartIcon />
+          <span className="header__optionLineTwo header__basketCount">0</span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Home.css";
+
+function Home() {
+  return (
+    <div className="home">
+      <div className="home__container">
+        <img src="https://images-eu.ssl-images-amazon.com/images/G/31/img20/Events/jupiter20/WAVE1-GW/6_DesktopHero_Unrec_3000x1200-aa._CB419164846_.jpg" />
+      </div>
+    </div>
+  );
+}
+
+export default Home;
